24582967cff6c925012f2bf714054764cbf0f2fe
woo <wbg030281@gmail.com>
2023-12-18 16:52:35 +0900

    변수 선언 및 초기화

---
+++ src/variable/Var1.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(20); //변경 10 -> 20
+        System.out.println(20); //만약 고칠 양이 1000개라면?
+        System.out.println(20); //변수(variable)을 사용
+    }
+}
---
+++ src/variable/Var3.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        //변수 선언 : 메모리 공간을 확보 하여 데이터 저장, 변수의 이름을 통하여 메모리 공간에 접근.
+        int a;
+        a = 10;
+        System.out.println("a = " + a);
+        
+        a = 50;// 변수 값 변경 a (10 -> 50)
+        System.out.println("a = " + a);
+    }
+}
