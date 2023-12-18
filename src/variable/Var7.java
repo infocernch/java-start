afbee68625387c2b0388258e792257a76969a558
woo <wbg030281@gmail.com>
2023-12-18 17:13:49 +0900

    변수 타입

---
+++ src/variable/Var7.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; //정수
+        double b = 10.5;//실수
+        boolean c = true;//불리언(boolean) true, false 입력가능
+        char d = 'A';//문자 하나
+        String e = "hello Java";//문자열, 문자열을 다루기 위한 특별한 타입
+
+        //개발자가 직접 적은 값들을 리터럴(literal) 이라한다.
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
