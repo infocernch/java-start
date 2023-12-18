de3855e6dd219579a55fc8f150b0bff154e8956c
woo <wbg030281@gmail.com>
2023-12-18 17:32:05 +0900

    변수 문제와 풀이

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long l = 100000000L;
+        System.out.println("l = " + l);
+        boolean b;
+        b = true;
+        System.out.println("b = " + b);
+    }
+}
